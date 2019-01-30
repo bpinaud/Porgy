ba71079e1a376f5fe087d6e5413a1405b8504bab
Bruno Pinaud <bruno.pinaud@u-bordeaux.fr>
2019-01-30 15:17:36 +0100

    add some working (or not) examples

---
+++ examples/update.py
@@ -0,0 +1,62 @@
+# Powered by Python 2.7
+
+# To cancel the modifications performed by the script
+# on the current graph, click on the undo button.
+
+# Some useful keyboards shortcuts : 
+#   * Ctrl + D : comment selected lines.
+#   * Ctrl + Shift + D  : uncomment selected lines.
+#   * Ctrl + I : indent selected lines.
+#   * Ctrl + Shift + I  : unindent selected lines.
+#   * Ctrl + Return  : run script.
+#   * Ctrl + F  : find selected text.
+#   * Ctrl + R  : replace selected text.
+#   * Ctrl + Space  : show auto-completion dialog.
+
+from tulip import *
+
+# the updateVisualization(centerViews = True) function can be called
+# during script execution to update the opened views
+
+# the pauseScript() function can be called to pause the script execution.
+# To resume the script execution, you will have to click on the "Run script " button.
+
+# the runGraphScript(scriptFile, graph) function can be called to launch another edited script on a tlp.Graph object.
+# The scriptFile parameter defines the script name to call (in the form [a-zA-Z0-9_]+.py)
+
+# the main(graph) function must be defined 
+# to run the script on the current graph
+
+def main(graph): 
+	NodeType = graph.getIntegerProperty("NodeType")
+	state = graph.getStringProperty("state")
+	viewBorderColor = graph.getColorProperty("viewBorderColor")
+	viewBorderWidth = graph.getDoubleProperty("viewBorderWidth")
+	viewColor = graph.getColorProperty("viewColor")
+	viewFont = graph.getStringProperty("viewFont")
+	viewFontAwesomeIcon = graph.getStringProperty("viewFontAwesomeIcon")
+	viewFontSize = graph.getIntegerProperty("viewFontSize")
+	viewLabel = graph.getStringProperty("viewLabel")
+	viewLabelBorderColor = graph.getColorProperty("viewLabelBorderColor")
+	viewLabelBorderWidth = graph.getDoubleProperty("viewLabelBorderWidth")
+	viewLabelColor = graph.getColorProperty("viewLabelColor")
+	viewLabelPosition = graph.getIntegerProperty("viewLabelPosition")
+	viewLabelRotation = graph.getDoubleProperty("viewLabelRotation")
+	viewLayout = graph.getLayoutProperty("viewLayout")
+	viewMetaGraph = graph.getGraphProperty("viewMetaGraph")
+	viewMetric = graph.getDoubleProperty("viewMetric")
+	viewRotation = graph.getDoubleProperty("viewRotation")
+	viewSelection = graph.getBooleanProperty("viewSelection")
+	viewShape = graph.getIntegerProperty("viewShape")
+	viewSize = graph.getSizeProperty("viewSize")
+	viewSrcAnchorShape = graph.getIntegerProperty("viewSrcAnchorShape")
+	viewSrcAnchorSize = graph.getSizeProperty("viewSrcAnchorSize")
+	viewTexture = graph.getStringProperty("viewTexture")
+	viewTgtAnchorShape = graph.getIntegerProperty("viewTgtAnchorShape")
+	viewTgtAnchorSize = graph.getSizeProperty("viewTgtAnchorSize")
+
+	for n in graph.getNodes():
+		if NodeType[n]==1:
+			for e in graph.getInEdges(n):
+				print "going to reverse edge ", e
+				graph.reverse(e)
