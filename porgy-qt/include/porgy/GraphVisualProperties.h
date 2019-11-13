546a106d7cf7d4108f1562a16325676eb566bca7
Bruno Pinaud <bruno.pinaud@u-bordeaux.fr>
2019-11-13 18:09:45 +0100

    remove unneeded code

--- porgy-qt/include/porgy/GraphVisualProperties.h
+++ porgy-qt/include/porgy/GraphVisualProperties.h
@@ -49,15 +49,6 @@ enum VisualProperties {
   AllVisualProperties = 0xffffffff
 };
 
-struct EnumClassHash
-{
-    template <typename T>
-    std::size_t operator()(T t) const
-    {
-        return static_cast<std::size_t>(t);
-    }
-};
-
 Q_DECLARE_FLAGS(VisualPropertiesFlags, VisualProperties)
 
 class PORGY_SCOPE GraphVisualProperties {
